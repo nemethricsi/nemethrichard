fa7d61ba20400937d0f40a7c6ca783909279529c
Richard Nemeth <nemethricsi@gmail.com>
2025-01-25 20:10:37 +0100

    refactor ModeToggle

--- app/components/mode-toggle.tsx
+++ app/components/mode-toggle.tsx
@@ -1,7 +1,12 @@
 'use client';
 
-import * as React from 'react';
-import { MoonStarIcon, MonitorIcon, SunIcon, CheckIcon } from 'lucide-react';
+import {
+  MoonStarIcon,
+  MonitorIcon,
+  SunIcon,
+  CheckIcon,
+  type LucideIcon,
+} from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
@@ -13,34 +18,50 @@ import {
 } from '@/components/ui/dropdown-menu';
 
 export function ModeToggle() {
-  const { setTheme, theme } = useTheme();
+  type themes = 'light' | 'dark' | 'system';
+
+  const menuItems: { name: themes; icon: LucideIcon }[] = [
+    {
+      name: 'light',
+      icon: SunIcon,
+    },
+    {
+      name: 'dark',
+      icon: MoonStarIcon,
+    },
+    {
+      name: 'system',
+      icon: MonitorIcon,
+    },
+  ];
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] scale-100 transition-all dark:scale-0" />
-          <MoonStarIcon className="absolute h-[1.2rem] w-[1.2rem] scale-0 transition-all dark:scale-100" />
+          <SunIcon className="size-5 scale-100 transition-all dark:scale-0" />
+          <MoonStarIcon className="absolute size-5 scale-0 transition-all dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          <SunIcon />
-          Light
-          {theme === 'light' && <CheckIcon className="ml-auto h-4 w-4" />}
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          <MoonStarIcon />
-          Dark
-          {theme === 'dark' && <CheckIcon className="ml-auto h-4 w-4" />}
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          <MonitorIcon />
-          System
-          {theme === 'system' && <CheckIcon className="ml-auto h-4 w-4" />}
-        </DropdownMenuItem>
+        {menuItems.map(({ name, icon }) => (
+          <MenuItem key={name} theme={name} icon={icon} />
+        ))}
       </DropdownMenuContent>
     </DropdownMenu>
   );
 }
+
+const MenuItem = ({ theme, icon }: { theme: string; icon: LucideIcon }) => {
+  const { setTheme, theme: currentTheme } = useTheme();
+  const Icon = icon;
+
+  return (
+    <DropdownMenuItem onClick={() => setTheme(theme)}>
+      <Icon />
+      <span className="capitalize">{theme}</span>
+      {currentTheme === theme && <CheckIcon className="ml-auto h-4 w-4" />}
+    </DropdownMenuItem>
+  );
+};
