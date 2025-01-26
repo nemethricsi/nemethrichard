2e0ed5783779607652ef0796eb719fc435e06aa7
Richard Nemeth <nemethricsi@gmail.com>
2025-01-26 14:54:20 +0100

    remove page props

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,9 +1,8 @@
-export default function ProjectPage({ params }: { params: { slug: string } }) {
+export default function ProjectPage() {
   return (
     <section className="pb-24 pt-24 sm:pt-40">
       <div className="container flex max-w-3xl flex-col gap-8">
         <h1 className="title">fmz.ngo</h1>
-        <pre>{JSON.stringify(params, null, 2)}</pre>
       </div>
     </section>
   );
